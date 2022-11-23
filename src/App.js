b67bd2eb33d57e5d9e6a19c26e80ab7c296da38d
meraj2085 <samirmeraj60@gmail.com>
2022-11-23 21:20:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
