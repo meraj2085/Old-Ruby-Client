1233ea5745f7d62d87ae9f1342dbd156adcbc8dd
meraj2085 <samirmeraj60@gmail.com>
2022-11-24 09:50:12 +0600

    adding .env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAI96soNYGw1HoBZ17emfu14yMSFl0mt4",
-  authDomain: "oldruby-91650.firebaseapp.com",
-  projectId: "oldruby-91650",
-  storageBucket: "oldruby-91650.appspot.com",
-  messagingSenderId: "52637366921",
-  appId: "1:52637366921:web:df99de06b3f041e775a458"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
