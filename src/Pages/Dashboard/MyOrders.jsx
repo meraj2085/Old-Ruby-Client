65584006967679a855c82f33b504f9d9f04a69f0
meraj2085 <samirmeraj60@gmail.com>
2022-11-25 18:59:02 +0600

    adding delete booking

--- src/Pages/Dashboard/MyOrders.jsx
+++ src/Pages/Dashboard/MyOrders.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import { itMatchesOne } from "daisyui/src/lib/postcss-prefixer/utils";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider";
+import BookingCancelModal from "../Shared/BookingCancelModal/BookingCancelModal";
 
 const MyOrders = () => {
-     return (
-          <div>
-               <h1>I am my orders</h1>
-          </div>
-     );
+  const { user } = useContext(AuthContext);
+  const [orders, setOrders] = useState(null);
+  const [toggle, setToggle] = useState(true);
+  const [deleteOrder, setDeleteOrder] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/booked?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, [user?.email, toggle]);
+
+  console.log(orders);
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold my-4 mx-5">My products</h1>
+      <div className="mx-5 shadow-md rounded-lg">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Cancel order</th>
+                <th>Payment</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders?.map((order, i) => (
+                <tr>
+                  <th>{i + 1}</th>
+                  <td>Cy Ganderton</td>
+                  <td>{order?.item_name}</td>
+                  <td>{order?.item_price}</td>
+                  <td>
+                    <label
+                      onClick={() => setDeleteOrder(order?._id)}
+                      htmlFor="ruby-cancelOrderModal"
+                      className="px-6 py-1 font-semibold border rounded hover:border-red-500 text-red-500"
+                    >
+                      Cancel
+                    </label>
+                  </td>
+                  <td>
+                    <button className="px-6 py-1 font-semibold border rounded hover:border-green-500 text-green-500">
+                      Pay
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {deleteOrder && (
+        <BookingCancelModal
+          setToggle={setToggle}
+          toggle={toggle}
+          deleteOrder={deleteOrder}
+          setDeleteOrder={setDeleteOrder}
+        ></BookingCancelModal>
+      )}
+    </div>
+  );
 };
 
 export default MyOrders;
---
+++ src/Pages/Shared/BookingCancelModal/BookingCancelModal.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import toast from "react-hot-toast";
+
+const BookingCancelModal = ({
+  toggle,
+  setToggle,
+  deleteOrder,
+  setDeleteOrder,
+}) => {
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/booking/${id}`, {
+     method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data =>{
+     if (data?.deletedCount){
+          setToggle(!toggle);
+          toast.success('Product booking cancelled', {duration: 2000})
+          setDeleteOrder(null)
+     }
+    })
+  };
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id="ruby-cancelOrderModal"
+        className="modal-toggle"
+      />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="ruby-cancelOrderModal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-2xl text-center font-semibold">
+            Are you sure you want to cancel?
+          </h3>
+          <div className="py-10 flex justify-evenly">
+            <button
+              onClick={() => handleDelete(deleteOrder)}
+              type="button"
+              className="px-8 py-3 font-semibold rounded-full bg-gray-800 text-gray-100"
+            >
+              Yes
+            </button>
+            <button
+              onClick={() => setDeleteOrder(null)}
+              type="button"
+              className="px-8 py-3 font-semibold rounded-full bg-gray-800 text-gray-100"
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingCancelModal;
