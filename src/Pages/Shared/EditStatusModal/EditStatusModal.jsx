e6361b8d6221905ab9082358af6fe3d0c102c0cb
meraj2085 <samirmeraj60@gmail.com>
2022-11-25 11:59:36 +0600

    adding modal

--- src/Pages/Shared/EditStatusModal/EditStatusModal.jsx
+++ src/Pages/Shared/EditStatusModal/EditStatusModal.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const EditStatusModal = () => {
   return (
     <div>
-      <input type="checkbox" id="ruby-editStatusModal" className="modal-toggle" />
+      <input
+        type="checkbox"
+        id="ruby-editStatusModal"
+        className="modal-toggle"
+      />
       <div className="modal">
         <div className="modal-box relative">
           <label
@@ -12,13 +16,26 @@ const EditStatusModal = () => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
+          <h3 className="text-2xl text-center font-semibold">
+            Is this product sold out?
           </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <div className="flex items-center mt-3 space-x-5">
+            <div className="flex-1 h-px sm:w-16 bg-gray-300"></div>
+          </div>
+          <div className="py-10 flex justify-evenly">
+            <button
+              type="button"
+              className="px-8 py-3 font-semibold rounded-full bg-gray-800 text-gray-100"
+            >
+              Sold out
+            </button>
+            <button
+              type="button"
+              className="px-8 py-3 font-semibold rounded-full bg-gray-800 text-gray-100"
+            >
+              Available
+            </button>
+          </div>
         </div>
       </div>
     </div>
