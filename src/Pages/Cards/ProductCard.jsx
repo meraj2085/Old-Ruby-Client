08a590ef202b5c0b602af006a341ef583ab9e285
meraj2085 <samirmeraj60@gmail.com>
2022-11-25 16:16:30 +0600

    adding report icon

--- src/Pages/Cards/ProductCard.jsx
+++ src/Pages/Cards/ProductCard.jsx
@@ -20,7 +20,25 @@ const ProductCard = ({ product, setProduct }) => {
         <img src={img} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <div className="flex justify-between">
+          <h2 className="card-title">{name}</h2>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-red-500"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+              />
+            </svg>
+          </div>
+        </div>
         <div className="flex">
           <div className="text-lg">Seller: {seller_name}</div>
           {seller_verification === true && (
@@ -34,7 +52,7 @@ const ProductCard = ({ product, setProduct }) => {
         <p>{description}</p>
         <div className="card-actions justify-between mt-4">
           <label
-          onClick={()=>setProduct(product)}
+            onClick={() => setProduct(product)}
             htmlFor="ruby-booking-modal"
             className="px-5 py-2 font-semibold border rounded border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-gray-100"
           >
--- src/Pages/CategoryProducts/ProductDetails.jsx
+++ src/Pages/CategoryProducts/ProductDetails.jsx
@@ -32,7 +32,25 @@ const ProductDetails = () => {
             <img src={img} alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">{name}</h2>
+            <div className="flex justify-between">
+              <h2 className="card-title">{name}</h2>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                  />
+                </svg>
+              </div>
+            </div>
             <div className="flex">
               <div className="text-lg">Seller: {seller_name}</div>
               {seller_verification === true && (
@@ -67,7 +85,10 @@ const ProductDetails = () => {
         </div>
       </div>
       {selectedProduct && (
-        <BookingModal product={product} setProduct={selectedProduct}></BookingModal>
+        <BookingModal
+          product={product}
+          setProduct={selectedProduct}
+        ></BookingModal>
       )}
     </div>
   );
