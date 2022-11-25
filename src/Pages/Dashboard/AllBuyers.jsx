1aa3dbab8f0349f6fce9ef4228cb97ee21a7668b
meraj2085 <samirmeraj60@gmail.com>
2022-11-25 15:29:56 +0600

    adding all sellers and all buyers

--- src/Pages/Dashboard/AllBuyers.jsx
+++ src/Pages/Dashboard/AllBuyers.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const AllBuyers = () => {
-     return (
-          <div>
-               <h1>I am all AllBuyers</h1>
-          </div>
-     );
+  const [buyers, setBuyers] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/users?role=Buyer")
+      .then((res) => res.json())
+      .then((data) => {
+        setBuyers(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold my-4 mx-5">All buyers</h1>
+      <div className="mx-5 shadow-md rounded-lg">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {buyers?.map((seller, i) => (
+                <tr>
+                  <th>{i + 1}</th>
+                  <td>{seller?.email}</td>
+                  <td>
+                    <button className="btn btn-xs">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllBuyers;
--- src/Pages/Dashboard/AllSellers.jsx
+++ src/Pages/Dashboard/AllSellers.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const AllSellers = () => {
-     return (
-          <div>
-               <h1>I am all sellers</h1>
-          </div>
-     );
+  const [sellers, setSellers] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/users?role=Seller")
+      .then((res) => res.json())
+      .then((data) => {
+        setSellers(data);
+      });
+  }, []);
+
+  console.log(sellers);
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold my-4 mx-5">All sellers</h1>
+      <div className="mx-5 shadow-md rounded-lg">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sellers?.map((seller, i) => (
+                <tr>
+                  <th>{i + 1}</th>
+                  <td>{seller?.email}</td>
+                  <td><button className="btn btn-xs">Delete</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllSellers;
