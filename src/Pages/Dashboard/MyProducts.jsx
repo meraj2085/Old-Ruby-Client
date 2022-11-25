af1987cf300645566861424da9c17b586af43ba6
meraj2085 <samirmeraj60@gmail.com>
2022-11-25 11:03:44 +0600

    adding my products api

--- src/Pages/Dashboard/MyProducts.jsx
+++ src/Pages/Dashboard/MyProducts.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider';
 
 const MyProducts = () => {
+     const {user} = useContext(AuthContext)
+     const [products, setProducts] = useState(null);
+     useEffect(()=>{
+          fetch(`http://localhost:5000/products?email=${user?.email}`)
+          .then(res => res.json())
+          .then(data => {
+               setProducts(data)
+          })
+     },[user])
+     console.log(products);
+     
      return (
           <div>
-               <h1>I am my products</h1>
+               <h1 className="text-3xl font-semibold my-4 mx-5">My products</h1>
           </div>
      );
 };
