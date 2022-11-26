8950af91c077e6adf49067b1c61bf285d947ea09
meraj2085 <samirmeraj60@gmail.com>
2022-11-26 11:14:46 +0600

    adding drawer btn

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import BuyerMenu from "../Pages/Menu/BuyerMenu";
 import SellerMenu from "../Pages/Menu/SellerMenu";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
+import AuthenticationSpinner from "../Pages/Shared/Spinners/AuthenticationSpinner";
 
 const DashboardLayout = () => {
   const { user, role, setRole } = useContext(AuthContext);
@@ -20,6 +21,10 @@ const DashboardLayout = () => {
     });
   }, [user, setRole]);
 
+  if (loading) {
+    return <AuthenticationSpinner></AuthenticationSpinner>;
+  }
+
   return (
     <>
       <Navbar></Navbar>
@@ -30,7 +35,7 @@ const DashboardLayout = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="ruby-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 text-base-content">
+          <ul className="menu p-4 w-80 sm:bg-base-100 md:bg-transparent text-base-content">
             <li>
               <Link to="/dashboard">Home</Link>
             </li>
--- src/Pages/Dashboard/WelcomePage.jsx
+++ src/Pages/Dashboard/WelcomePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const WelcomePage = () => {
-     return (
-          <div>
-               <h1>I am welcome page</h1>
-          </div>
-     );
+  return (
+    <div>
+      <div className="flex justify-between mx-5 my-5">
+        <h1>I am welcome page</h1>
+        <label htmlFor="ruby-drawer" className="drawer-button lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+            />
+          </svg>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
