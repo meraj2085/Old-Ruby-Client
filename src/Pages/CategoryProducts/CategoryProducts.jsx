93254f74e5052990381dad723f855aa6ea466867
meraj2085 <samirmeraj60@gmail.com>
2022-11-26 10:50:05 +0600

    adding optional chaining in category

--- src/Pages/CategoryProducts/CategoryProducts.jsx
+++ src/Pages/CategoryProducts/CategoryProducts.jsx
@@ -13,7 +13,7 @@ const CategoryProducts = () => {
     <div className="my-16">
       <div className="p-4 mb-10 mx-auto text-center md:px-10 lg:px-32 xl:max-w-3xl">
         <h2 className="text-5xl font-semibold leading-none sm:text-4xl">
-          <span className="text-red-700">{products[0].category} C</span>ategory
+          <span className="text-red-700">{products[0]?.category} C</span>ategory
         </h2>
         <div className="flex items-center pt-4 space-x-1">
           <div className="flex-1 h-px sm:w-16 bg-gray-300"></div>
@@ -33,7 +33,12 @@ const CategoryProducts = () => {
       {product && (
         <BookingModal product={product} setProduct={setProduct}></BookingModal>
       )}
-      {reportedItem && <ReportModal reportedItem={reportedItem} setReportedItem={setReportedItem}></ReportModal>}
+      {reportedItem && (
+        <ReportModal
+          reportedItem={reportedItem}
+          setReportedItem={setReportedItem}
+        ></ReportModal>
+      )}
     </div>
   );
 };
