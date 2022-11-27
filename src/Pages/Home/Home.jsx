3d4c53a12a7026d7b6cbfd0a2484983b1d4c23cb
meraj2085 <samirmeraj60@gmail.com>
2022-11-27 09:20:58 +0600

    adding home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,29 +16,42 @@ const Home = () => {
 
   return (
     <div>
-      <div className="px-4 py-16 mx-auto min-h-screen sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
-          <div className="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
-            <div className="mb-6">
-              <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none max-w-lg mb-6">
-                Sell Your old items
-                <br className="hidden md:block" />
-                or buy from here{" "}
-              </h2>
-              <p className="text-gray-700 text-base md:text-lg">
-                I am a professional home chef. Committed to provide best meal
-                for you in minimal time. I have been cooking for 15 years &
-                worked with several renowned restaurants. Now i myself will cook
-                personally for you and provide you your personal dish.
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center justify-center lg:w-1/2">
-            <div className="lg:w-[750px] w-[300px]">
-              <img className="object-cover" src={sellingImg} alt="" />
-            </div>
-          </div>
+      <div className="flex flex-col items-center justify-center max-w-2xl px-4 pt-7 mx-auto sm:max-w-xl md:max-w-2xl lg:pt-20 md:px-8">
+        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-5">
+          <h2 className="max-w-xl text-center mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg
+                viewBox="0 0 52 24"
+                fill="currentColor"
+                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              >
+                <defs>
+                  <pattern
+                    id="9ef1ff62-feb2-41fe-8163-772b4c79de7b"
+                    x="0"
+                    y="0"
+                    width=".135"
+                    height=".30"
+                  >
+                    <circle cx="1" cy="1" r=".7" />
+                  </pattern>
+                </defs>
+                <rect
+                  fill="url(#9ef1ff62-feb2-41fe-8163-772b4c79de7b)"
+                  width="52"
+                  height="24"
+                />
+              </svg>
+              <span className="relative text-4xl font-bold sm:text-6xl">Buy</span>
+            </span>{" "}
+            <span className="text-4xl font-bold sm:text-6xl">what your desire</span>
+          </h2>
+          <p className="text-base text-gray-700 md:text-lg text-center">
+          Here you can sell your unused mobile phone or you can buy form here.
+          Choose your favourite product and deal directly with the seller
+        </p>
         </div>
+        <img src={sellingImg} className="w-full mx-auto md:w-auto" alt="" />
       </div>
       <Catagories></Catagories>
       {advertisedProduct?.length > 0 && (
