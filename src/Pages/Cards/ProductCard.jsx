24a36f3c0631eb7ce16ff7412454283336a802a7
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-11-23 09:07:16 +0600

    Updating prodct details

--- src/Pages/Cards/ProductCard.jsx
+++ src/Pages/Cards/ProductCard.jsx
@@ -16,15 +16,14 @@ const ProductCard = ({
     seller_verification,
     description,
     location,
-    _id
+    _id,
   } = product;
-
   return (
     <div className="card bg-base-100 shadow-xl">
-      <figure>
-        <img src={img} alt="Shoes" />
+      <figure className="h-[150px]">
+        <img src={img} alt="Product" />
       </figure>
-      <div className="card-body">
+      <div className="card-body gap-0">
         <div className="flex justify-between">
           <h2 className="card-title">{name}</h2>
           <div>
@@ -38,7 +37,7 @@ const ProductCard = ({
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6 text-red-500"
+                className="w-5 h-5 text-red-500"
               >
                 <path
                   strokeLinecap="round"
@@ -50,31 +49,71 @@ const ProductCard = ({
           </div>
         </div>
         <div className="flex">
-          <div className="text-lg">Seller: {seller_name}</div>
+          <div className="text-sm">{seller_name}</div>
           {seller_verification === true && (
             <div>
-              <img src={badge} className="w-6 mx-3" alt="" />
+              <img src={badge} className="w-3 mt-[5px] mx-1" alt="" />
             </div>
           )}
         </div>
-        <p>Location: {location}</p>
-        <p>Price: ${resell_price}</p>
-        <p>{description}</p>
-        <div className="card-actions justify-between mt-4">
-          <label
-            onClick={() => setProduct(product)}
-            htmlFor="ruby-booking-modal"
-            className="px-5 py-2 font-semibold border rounded border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-gray-100"
-          >
-            Book Now
-          </label>
-          <Link to={`/product/${_id}`}>
-            <button
-              type="button"
-              className="px-5 py-2 font-semibold border rounded border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-gray-100"
+        <p className="mt-5">
+          {description.length > 50
+            ? `${description.slice(0, 50)}...`
+            : description}
+        </p>
+        <p className="flex gap-2 mt-3">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-map-pin"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              View Details
-            </button>
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+              <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+            </svg>
+          </span>{" "}
+          {location}
+        </p>
+        <p className="flex gap-2">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-currency-dollar"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+              <path d="M12 3v3m0 12v3" />
+            </svg>
+          </span>
+          ${resell_price}
+        </p>
+        <div className="button_part flex justify-end">
+          <Link to={`/product/${_id}`}>
+            <p className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-[#b23a48]">
+              See details
+              <span
+                aria-hidden="true"
+                className="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+              >
+                &rarr;
+              </span>
+            </p>
           </Link>
         </div>
       </div>
@@ -83,3 +122,5 @@ const ProductCard = ({
 };
 
 export default ProductCard;
+
+
--- src/Pages/CategoryProducts/CategoryProducts.jsx
+++ src/Pages/CategoryProducts/CategoryProducts.jsx
@@ -10,16 +10,20 @@ const CategoryProducts = () => {
   const [reportedItem, setReportedItem] = useState(null);
 
   return (
-    <div className="my-16">
+    <div className="my-16 max-w-[1200px] mx-auto">
       <div className="p-4 mb-10 mx-auto text-center md:px-10 lg:px-32 xl:max-w-3xl">
         <h2 className="text-5xl font-semibold leading-none sm:text-4xl">
-          <span className="text-red-700">{products[0]?.category} C</span>ategory
+          <span className="text-red-700">
+            {products[0]?.category.charAt(0).toUpperCase() +
+              products[0]?.category.slice(1)}{" "}
+          </span>
+          Category
         </h2>
         <div className="flex items-center pt-4 space-x-1">
           <div className="flex-1 h-px sm:w-16 bg-gray-300"></div>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mx-5 lg:mx-16">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-5">
         {products?.map((product) => (
           <ProductCard
             product={product}
